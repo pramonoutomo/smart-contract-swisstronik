cff1a3df2dadb2f9c385f63e8882d8a8f404f0f8
Pramono Utomo <emailbuatcariduit@gmail.com>
2023-07-05 16:56:58 +0700

    Update smart contract

--- Register.sol
+++ Register.sol
@@ -1,1 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Register {
+    string public github;
+    address public owner;
+    Referral[] public referrals;
+
+    struct Referral {
+        address referralAddress;
+        string referralString;
+    }
+
+    constructor() {
+        github = "pramonoutomo";
+        owner = 0x3a9Fbef0c5A2712aCf48d5b07730a584A8Af028A;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can call this function");
+        _;
+    }
+
+    function addReferral(address _referralAddress, string memory _referralString) external onlyOwner {
+        Referral memory newReferral = Referral(_referralAddress, _referralString);
+        referrals.push(newReferral);
+    }
+
+    function totalReferrals() public view returns (uint256) {
+        return referrals.length;
+    }
+}
 
